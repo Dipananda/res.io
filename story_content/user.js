5cb29b29334f4ab2099d9856df09deddf70528ca
Dipananda <71223670+Dipananda@users.noreply.github.com>
2023-10-09 12:06:20 +0530

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,21 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6Sb0kAS0CwU":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  if (document.seen !== 1) {
+ document.seen = 1;
+ let wrapper = document.querySelector('#wrapper');
+ let slide = document.querySelector('#slide');
+ let target = document.querySelector('#preso');
+ target.style.transformOrigin = '50% 0';
+ target.style.transform = 'scale('+(wrapper.offsetHeight/slide.offsetHeight)+')';
+ }
 }
 
